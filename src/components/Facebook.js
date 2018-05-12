c270b3c4616fa54d592f050f79918bc1fa6d8191
Stephan Griesel <sgriesel@gmail.com>
2018-05-12 10:20:34 +0200

    app done

--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -11,8 +11,16 @@ export default class componentName extends Component {
     }
 
     responseFacebook = response => {
-        console.log(response);
-    }
+       console.log(response);
+
+       this.setState({
+           isLoggedIn: true,
+           userID: response.userID,
+           name: response.name,
+           email: response.email,
+           picture: response.picture.data.url
+       });
+    };
     
     componentClicked = () => console.log("clicked");
 
@@ -20,7 +28,18 @@ export default class componentName extends Component {
       let fbContent;
 
       if(this.state.isLoggedIn) {
-          fbContent = null;
+          fbContent = (
+              <div style={{
+                  width: '400px',
+                  margin: 'auto',
+                  background: '#f4f4f4',
+                  padding: '20px'                
+              }}>
+                <img src={this.state.picture} alt={this.state.name} />
+                <h2>Welcome {this.state.name}</h2>
+                Email: {this.state.email}
+              </div>
+          );
       } else {
           fbContent = (<FacebookLogin
             appId="2014065428857541"
